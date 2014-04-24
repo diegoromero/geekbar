bf6db7d1d6d75b71da01dbeb508ea4afad8cf5c7
Diego Romero <diego.romerop@gmail.com>
2014-04-24 17:12:56 -0430

    newer quantity look

--- orders/static/js/orders.js
+++ orders/static/js/orders.js
@@ -1,7 +1,18 @@
 $(document).on('pageshow', function() {
-	/*$('.carousel').slick({
-		arrows: false
-	});*/
 	$('.carousel').owlCarousel();
 	$('.rm_h').show();
+	$('.plus').on('click', function() {
+		var quant = parseInt($(this).siblings('.quantity').text());
+		quant++;
+		$(this).siblings('.quantity').text(quant.toString());
+		$(this).parents('.fs').children('form').children('input[name=quantity]').val(quant);
+	});
+	$('.minus').on('click', function() {
+		var quant = parseInt($(this).siblings('.quantity').text());
+		if (quant > 1) {
+			quant--;
+			$(this).siblings('.quantity').text(quant.toString());
+			$(this).parents('.fs').children('form').children('input[name=quantity]').val(quant);
+		};
+	});
 });
