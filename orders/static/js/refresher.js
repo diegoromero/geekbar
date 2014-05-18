b5b20d5b43ab6f9d8cc0d5e3ce688bc0c6ba411d
Diego Romero <diego.romerop@gmail.com>
2014-05-18 11:05:34 -0430

    controls

--- orders/static/js/refresher.js
+++ orders/static/js/refresher.js
@@ -1,11 +1,11 @@
 var refresh_rate = 7830;
 
-function poll(){
-	setTimeout(function(){
-		$('.refresh_container').load('screen_refresh/', function() {poll();});
-	}, refresh_rate);
-};
+
 
 $(document).ready(function() {
-	poll();
+	(function poll(){
+		setTimeout(function(){
+			$('.refresh_container').load('screen_refresh/', function() {poll();});
+		}, refresh_rate);
+	})();
 });
