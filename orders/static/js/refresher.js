bba8212103b4f614aa960d7aa83aa066109c9342
diegoromero <diego.romerop@gmail.com>
2014-05-20 11:59:19 -0430

    refresher testing

--- orders/static/js/refresher.js
+++ orders/static/js/refresher.js
@@ -2,10 +2,12 @@
 
 
 var refresh_rate = 7830;
+var refresh_active = true;
 
 (function poll(){
 	setTimeout(function(){
-		$('.refresh_wrapper').load('screen_refresh/ .refresh_container', function() {
+		$('.ui-page-active .refresh_wrapper').load('screen_refresh/ .refresh_container', function() {
+			refresh_active = true;
 			$('.refresh_container').trigger( "create" );
 			console.log('refreshed');
 			poll();
@@ -13,4 +15,13 @@ var refresh_rate = 7830;
 	}, refresh_rate);
 })();
 
-
+$("document").on("pageshow",function(event){
+	console.log('pageshow');
+});
+$(document).on("pagecreate",function(event){
+  console.log('pagecreate');
+  console.log(refresh_active);
+});
+$(document).on("pagecontainerload",function(event,data){
+  console.log('pagecontainerload');
+});
