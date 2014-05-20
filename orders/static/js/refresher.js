ce619c50e42594e9feb71ae309fe5f7d4e665095
Diego Romero <diego.romerop@gmail.com>
2014-05-20 12:29:25 -0430

    refresher ready

--- orders/static/js/refresher.js
+++ orders/static/js/refresher.js
@@ -1,27 +1,9 @@
-//refresh suspended while dev
-
-
 var refresh_rate = 7830;
-var refresh_active = true;
 
-(function poll(){
-	setTimeout(function(){
-		$('.ui-page-active .refresh_wrapper').load('screen_refresh/ .refresh_container', function() {
-			refresh_active = true;
-			$('.refresh_container').trigger( "create" );
-			console.log('refreshed');
-			poll();
-		});
-	}, refresh_rate);
-})();
+function poll(){
+	$('.ui-page-active .refresh_wrapper').load('screen_refresh/ .refresh_container', function(response, status, xml) {
+		$('.refresh_container').trigger( "create" );
+	});
+};
 
-$("document").on("pageshow",function(event){
-	console.log('pageshow');
-});
-$(document).on("pagecreate",function(event){
-  console.log('pagecreate');
-  console.log(refresh_active);
-});
-$(document).on("pagecontainerload",function(event,data){
-  console.log('pagecontainerload');
-});
+setInterval(poll, refresh_rate);
